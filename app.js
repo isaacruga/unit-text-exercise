f8a267f35d65e50b9272e752e3917e65e197dc01
isaacruizgavilan4geeks <isaacruizgavilan@me.com>
2021-10-09 11:13:34 +0000

    Modified changes in app.js

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const sum = (a,b) => {
     return a + b
 }
 
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
 // we declare the function with the exact name "fromEuroToDollar"
 const fromEuroToDollar = function(valueInEuro){
-    return valueInEuro * 1.2;
+    return valueInEuro * oneEuroIs.USD;
 }
 
 // function fromDollarToYen
-const fromDollarToYen = function(valueInEuro){
-    return (valueInEuro / 1.2) * 127.9;
+const fromDollarToYen = function(valueInDollar){
+    return (valueInDollar / oneEuroIs.USD) * oneEuroIs.JPY;
 }
 
 // function fromYenToPound
-const fromYenToPound = function (valueInEuro){
-    return (valueInEuro / 127.9) * 0.8;
+const fromYenToPound = function (valueInYen){
+    return (valueInYen / oneEuroIs.JPY) * oneEuroIs.GBP;
 }
 
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
